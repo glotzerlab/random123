6ece94f31c2a8e096d4c7be1dcb75531213ffb27
Jens Glaser <jsglaser@umich.edu>
2020-03-02 12:16:34 -0500

    enable clang compile on power
    https://github.com/opnsense/ports/blob/master/astro/oskar/files/patch-extern_Random123_features_gccfeatures.h

--- include/Random123/features/gccfeatures.h
+++ include/Random123/features/gccfeatures.h
@@ -44,7 +44,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  Please let the authors know of any successes (or failures). */
 #endif
 
-#ifdef __powerpc__
+#if defined(__powerpc__) && !defined(__clang__)
 #include <ppu_intrinsics.h>
 #endif
 
@@ -228,6 +228,20 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #else
 #define R123_USE_MULHILO64_MULHI_INTRIN 0
 #endif
+#endif
+
+#if defined(__powerpc__) && defined(__clang__)
+#ifdef __powerpc64__
+static inline unsigned long long __mulhdu(unsigned long long a, unsigned long long b) {
+  __uint128_t c = (__uint128_t) a * (__uint128_t) b;
+  return c >> 64;
+}
+#endif
+
+static inline unsigned int __mulhwu(unsigned int a, unsigned int b) {
+  unsigned long long c = (unsigned long long) a * (unsigned long long) b;
+  return c >> 32;
+}
 #endif
 
 #ifndef R123_MULHILO64_MULHI_INTRIN
